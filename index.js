5fc3fe8543ddad2cfa6864052dc38804da21dbe2
vhumeniak <144223972+vhumeniak@users.noreply.github.com>
2023-11-19 22:07:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let arr_info = []
+
+arr_info[0] = parseInt('2004', 29)
+arr_info[1] = 18
+arr_info[2] = undefined
+arr_info[3] = null
+arr_info[4] = "Humeniak Vasyl"
+
+
+let User_User =
+{
+    name_user : arr_info[4],
+    age : arr_info[1],
+    year_born : arr_info[0]
+}
+
+console.table({arr_info})
+console.table({User_User})   
